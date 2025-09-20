b2f578e547cba52149fc0c935bb6b738399e20a2
AtharvBolke99 <atharvbolke99@gmail.com>
2025-09-20 23:42:36 +0530

    minor changes done

--- javascript/home.js
+++ javascript/home.js
@@ -6,8 +6,8 @@ const togglemainu = () => {
 const appsCard = document.getElementById("apps-card");
 const overlay = document.getElementById("overlay");
 const img = document.getElementById("appImg");
-let cardArray = [{img:"img0.jpg", appName:"Expense Tracker", appLines:"Easily log daily expenses, categorize spending, and visualize your budget with clear insights."},
-                 {img:"img1.jpg", appName:"Quiz App", appLines:"Challenge yourself with fun quizzes across multiple categories.", link:""},
+let cardArray = [{img:"img0.jpg", appName:"Expense Tracker", appLines:"Easily log daily expenses, categorize spending, and visualize your budget with clear insights.", link:""},
+                 {img:"img1.jpg", appName:"Quiz App", appLines:"Challenge yourself with fun quizzes across multiple categories.", link:"tic.html"},
                  {img:"img2.jpg", appName:"Todo List", appLines:"Easily create, manage, and prioritize tasks in one place.", link:""},
                  {img:"", appName:"Build Calculator", appLines:"Perform basic and advanced calculations with ease SmartCalc is your reliable everyday.", link:""},
                  {img:"", appName:"BMI Calculator", appLines:"Quickly calculate your Body Mass Index to understand your fitness level.", link:""},
@@ -15,7 +15,7 @@ let cardArray = [{img:"img0.jpg", appName:"Expense Tracker", appLines:"Easily lo
                  {img:"", appName:"Digital Clock", appLines:"A clean and modern digital clock for your screen.", link:""},
                  {img:"", appName:"Calender", appLines:"Keep track of important dates, events, and reminders.", link:""},
     ];
-for(let i=1; i<cardArray.length;i++){
+for(let i=0; i<cardArray.length;i++){
     appsCard.innerHTML += `
     <div class="apps" id="apps-card">
     <div class="apps-card">
@@ -23,20 +23,10 @@ for(let i=1; i<cardArray.length;i++){
         <h3 class="app-name">${cardArray[i].appName}</h3>
         <P class="app-lines">${cardArray[i].appLines}</P>
         <div class="btn">
-            <button class="start-btn">Start Now</button>
+            <button class="start-btn" onclick="showOverlay()"><a href="./pages/games/${cardArray[i].link}"}>Start Now</a></button>
         </div>
     </div>
     </div>
     `;
-function showOverlay() {   
-    overlay.style.display = 'flex';
-    overlay.innerHTML = `
-    <div class="overlay" id="overlay">
-                <a href="${cardArray[i].link}"></a>
-            </div>
-    `;
-
 }
-}
-
 
