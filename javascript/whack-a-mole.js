a7bda7d7ef7e08fb1774624067143123ac3ef06c
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-22 16:36:33 +0530

    var created

---
+++ javascript/whack-a-mole.js
@@ -0,0 +1,11 @@
+        let score = 0;
+        let timeLeft = 60;
+        let gameActive = false;
+        let gamePaused = false;
+        let level = 1;
+        let moleInterval;
+        let timeInterval;
+        let currentMole = null;
+        let moleSpeed = 1500; 
+
+        
