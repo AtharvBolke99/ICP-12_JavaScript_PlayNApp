682b8d2438a398599d3a53ca96f584449509fdfd
AtharvBolke99 <atharvbolke99@gmail.com>
2025-09-23 22:06:19 +0530

    linking completed

--- javascript/home.js
+++ javascript/home.js
@@ -6,13 +6,13 @@ const togglemainu = () => {
 const appsCard = document.getElementById("apps-card");
 const img = document.getElementById("appImg");
 let cardArray = [{img:"expense.png", appName:"Expense Tracker", appLines:"Easily log daily expenses and visualize your budget with clear insights.", link:"expensetraker.html"},
-                 {img:"quiz.png", appName:"Quiz App", appLines:"Challenge yourself with fun quizzes across multiple categories.", link:""},
+                 {img:"quiz.png", appName:"Quiz App", appLines:"Challenge yourself with fun quizzes across multiple categories.", link:"quizApp.html"},
                  {img:"todo.png", appName:"Todo List", appLines:"Easily create, manage, and prioritize tasks in one place.", link:"todo.html"},
                  {img:"calculator.jpg", appName:"Build Calculator", appLines:"Perform advanced calculations with ease SmartCalc is your reliable everyday.", link:"calculator.html"},
                  {img:"bmi_calculator.png", appName:"BMI Calculator", appLines:"Quickly calculate your Body Mass Index to understand your fitness level.", link:"bmical.html"},
                  {img:"text_op.jpg", appName:"Text Operation", appLines:"Easily transform, format, and analyze your text with handy tools.", link:"text.html"},
                  {img:"clock.jpg", appName:"Digital Clock", appLines:"A clean and modern digital clock for your screen.", link:"clock.html"},
-                 {img:"calender.jpg", appName:"Calender", appLines:"Keep track of important dates, events, and reminders.", link:""},
+                 {img:"calender.jpg", appName:"Calender", appLines:"Keep track of important dates, events, and reminders.", link:"calender.html"},
                  {img:"number_sys.png", appName:"Number System", appLines:"Easily convert and learn different number systems.", link:"number-system.html"},
                  {img:"torch.jpg", appName:"Torch", appLines:"Light up your way with a simple digital torch.", link:"torch.html"},
     ];
@@ -36,8 +36,8 @@ const gameimg = document.getElementById("gameImg");
 let gameArray = [{gameimg:"tic_tac_toe.jpg", gameName:"Tic-Tac-Toe", gameLines:"Play the classic X and O game and test your strategy.", link:"tic-tac-toe.html"},
                  {gameimg:"guess_number.jpg", gameName:"Guess A Number", gameLines:"Challenge your mind to guess the hidden number.", link:"guessNumber.html"},
                  {gameimg:"color_guess.jpg", gameName:"Colour Guessing Game", gameLines:"Test your color sense with a fun guessing challenge.", link:"color-guessing.html"},
-                 {gameimg:"hangman.jpg", gameName:"Hangman Game", gameLines:"Guess the word before the hangman is complete.", link:""},
-                 {gameimg:"whack-a-mole.jpg", gameName:"Whack-A-Mole", gameLines:"Tap fast and score by catching popping moles.", link:""},
+                 {gameimg:"hangman.jpg", gameName:"Hangman Game", gameLines:"Guess the word before the hangman is complete.", link:"hangman.html"},
+                 {gameimg:"whack-a-mole.jpg", gameName:"Whack-A-Mole", gameLines:"Tap fast and score by catching popping moles.", link:"whack-a-mole.html"},
     ];
 for(let i=0; i<gameArray.length;i++){
     gameCard.innerHTML += `
