ab915c5416d0deebe9f2d83ce0be73981cdd5e7f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-23 20:41:54 +0530

    Html elements taken

---
+++ javascript/hangman.js
@@ -0,0 +1,6 @@
+const wordDisplayEl = document.getElementById('word-display');
+const wrongGuessesEl = document.getElementById('wrong-guesses').querySelector('span');
+const gameMessageEl = document.getElementById('game-message');
+const keyboardEl = document.getElementById('keyboard');
+const restartButton = document.getElementById('restart-button');
+const figureParts = document.querySelectorAll('.figure-part');
